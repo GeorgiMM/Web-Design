cc407bd2b581ad511a6cc9ae83af5b58eabb01a2
GeorgiMM <georgi_metodiev_2@abv.bg>
2018-09-02 23:58:39 +0300

    Red changed with Green

--- GMM-WD-course-work/app-js/app.js
+++ GMM-WD-course-work/app-js/app.js
@@ -2,13 +2,13 @@ var color;
 
 
 
-document.querySelector('#colorbtn').addEventListener('click', test);
-  function  test(){
+document.querySelector('#colorbtn').addEventListener('click', changeColor);
+  function  changeColor(){
     color = document.querySelector('#colorbtn').textContent;
     if (color == "Blue") {
-    document.querySelector('.navigation').style.background='linear-gradient(to bottom right, red , orange)';
-    document.querySelector('#colorbtn').textContent = "Red";
-  } else if(color == "Red") {
+    document.querySelector('.navigation').style.background='linear-gradient(to bottom right, rgb(62, 186, 19) , rgb(172, 203, 19))';
+    document.querySelector('#colorbtn').textContent = "Green";
+  } else if(color == "Green") {
     document.querySelector('.navigation').style.background='linear-gradient(to bottom right, blue , aqua)';
     document.querySelector('#colorbtn').textContent = "Blue";
   }
