322c5f3293f8bd88b2ba2c3114a75eb137acc87a
GeorgiMM <georgi_metodiev_2@abv.bg>
2018-09-02 23:40:34 +0300

    Works twice

--- GMM-WD-course-work/app-js/app.js
+++ GMM-WD-course-work/app-js/app.js
@@ -2,12 +2,14 @@ var color = document.querySelector('#colorbtn').textContent;
 
 
 
-document.querySelector('#colorbtn').addEventListener('click', function  (){
-  if (color == "Blue") {
+document.querySelector('#colorbtn').addEventListener('click', test);
+  function  test(){
+    color = document.querySelector('#colorbtn').textContent;
+    if (color == "Blue") {
     document.querySelector('.navigation').style.background='linear-gradient(to bottom right, red , orange)';
     document.querySelector('#colorbtn').textContent = "Red";
   } else if(color == "Red") {
     document.querySelector('.navigation').style.background='linear-gradient(to bottom right, blue , aqua)';
     document.querySelector('.colorbtn').textContent = "Blue";
   }
-} );
+}
