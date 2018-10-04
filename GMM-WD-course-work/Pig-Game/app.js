54ebe3594db8c455399d844c95129c0d14cb1e8f
GeorgiMM <georgi_metodiev_2@abv.bg>
2018-10-04 14:31:34 +0300

    Finish

--- GMM-WD-course-work/Pig-Game/app.js
+++ GMM-WD-course-work/Pig-Game/app.js
@@ -11,14 +11,7 @@ GAME RULES:
 var scores,roundScore,activePlayer,dice;
 var diceDOM = document.querySelector('.dice');
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+init();
 
 function btn(){
   dice = Math.floor((Math.random() * 6) + 1);
@@ -40,8 +33,11 @@ document.querySelector('.dice').style.display = 'none';
 document.querySelector('.btn-roll').addEventListener('click', btn);
 
 document.querySelector('.btn-hold').addEventListener('click', function(){
+
   scores[activePlayer] += roundScore;
+
   document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
   if (scores[activePlayer] >= 100) {
     document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
     document.querySelector('.dice').style.display = 'none';
@@ -65,4 +61,23 @@ function nextPlayer (){
       document.querySelector('.player-1-panel').classList.toggle('active');
 
       diceDOM.style.display = 'none';
+}
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init(){
+  scores = [0,0];
+  roundScore = 0;
+  activePlayer = 0;
+
+  document.getElementById('score-0').textContent = '0';
+  document.getElementById('score-1').textContent = '0';
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+  document.querySelector('#name-0').textContent = 'Player 1';
+  document.querySelector('#name-1').textContent = 'Player 2';
+  document.querySelector('.player-0-panel').classList.remove('winner');
+  document.querySelector('.player-1-panel').classList.remove('winner');
+  document.querySelector('.player-0-panel').classList.add('active');
+
 }
