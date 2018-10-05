ba22686b153fbd779dc8e61f8905cab06089303d
GeorgiMM <georgi_metodiev_2@abv.bg>
2018-10-05 10:31:01 +0300

    Dice new game fix

--- GMM-WD-course-work/Pig-Game/app.js
+++ GMM-WD-course-work/Pig-Game/app.js
@@ -88,5 +88,6 @@ function init(){
   document.querySelector('.player-1-panel').classList.remove('active');
   document.querySelector('.player-1-panel').classList.remove('active');
   document.querySelector('.player-0-panel').classList.add('active');
+  diceDOM.style.display = 'none';
 
 }
